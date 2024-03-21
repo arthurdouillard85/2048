87f14dec2e87325b3119edb9d8c04b6923523e17
Arthur <arthur.douillard85@gmail.com>
2024-03-21 08:48:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a2048.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
