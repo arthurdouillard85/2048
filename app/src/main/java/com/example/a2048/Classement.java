a936efac0be3edb19b2f4f4287255779a06bf8f7
Arthur <arthur.douillard85@gmail.com>
2024-03-26 12:21:14 +0100

    Corr

--- app/src/main/java/com/example/a2048/Classement.java
+++ app/src/main/java/com/example/a2048/Classement.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class classement extends AppCompatActivity {
+public class Classement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/a2048/ScoreJoueur.java
+++ app/src/main/java/com/example/a2048/ScoreJoueur.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link score_joueur#newInstance} factory method to
+ * Use the {@link ScoreJoueur#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class score_joueur extends Fragment {
+public class ScoreJoueur extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class score_joueur extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public score_joueur() {
+    public ScoreJoueur() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class score_joueur extends Fragment {
      * @return A new instance of fragment score_joueur.
      */
     // TODO: Rename and change types and number of parameters
-    public static score_joueur newInstance(String param1, String param2) {
-        score_joueur fragment = new score_joueur();
+    public static ScoreJoueur newInstance(String param1, String param2) {
+        ScoreJoueur fragment = new ScoreJoueur();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
