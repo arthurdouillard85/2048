c76feaf04710dca2f727f0d3ac75292c87544649
loangrmd <loangermond2@gmail.com>
2024-04-08 16:07:04 +0200

    Ajout des logos

--- app/src/main/java/com/example/a2048/Classement.java
+++ app/src/main/java/com/example/a2048/Classement.java
@@ -1,14 +1,20 @@
 package com.example.a2048;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+
+        //import com.example.a2048.databinding.ActivityAccueilBinding;
+        import com.example.a2048.databinding.ActivityClassementBinding;
 
 public class Classement extends AppCompatActivity {
 
+    private ActivityClassementBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_classement);
+
+        binding = ActivityClassementBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/example/a2048/Jeux.java
+++ app/src/main/java/com/example/a2048/Jeux.java
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.a2048.databinding.ActivityJeuxBinding;
 import com.example.a2048.databinding.ActivityMainBinding;
 
 public class Jeux extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivityJeuxBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityJeuxBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
