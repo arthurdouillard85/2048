534ad2c887728badc3476dc427b4c78fbbec017d
bastienmerlet <bastienmerlet49@gmail.com>
2024-04-11 10:33:38 +0200

    Jeux V2

--- app/src/main/java/com/example/a2048/Classement.java
+++ app/src/main/java/com/example/a2048/Classement.java
@@ -1,6 +1,7 @@
 package com.example.a2048;
 
         import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.FragmentTransaction;
 
         import android.content.Intent;
         import android.os.Bundle;
@@ -8,15 +9,31 @@ package com.example.a2048;
         //import com.example.a2048.databinding.ActivityAccueilBinding;
         import com.example.a2048.databinding.ActivityClassementBinding;
 
+        import java.util.ArrayList;
+        import java.util.List;
+
 public class Classement extends AppCompatActivity {
 
     private ActivityClassementBinding binding;
+    private List<ScoreJoueur> fragments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityClassementBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setContentView(R.layout.fragment_score_joueur);
+        fragments = new ArrayList<>();
+        fragments.add(ScoreJoueur.newInstance("1", "Bastien", 10001, 2048));
+        fragments.add(ScoreJoueur.newInstance("2", "Loan", 10001, 2));
+        fragments.add(ScoreJoueur.newInstance("3", "Arthur", 10001, 4));
+        fragments.add(ScoreJoueur.newInstance("4", "Armoif", 10001, 8));
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        for(ScoreJoueur frag : fragments) {
+            ft.add(R.id.fragment_container,frag);
+        }
+        ft.commit();
     }
 
     @Override
--- app/src/main/java/com/example/a2048/ScoreJoueur.java
+++ app/src/main/java/com/example/a2048/ScoreJoueur.java
@@ -2,11 +2,14 @@ package com.example.a2048;
 
 import android.os.Bundle;
 
+import androidx.annotation.DrawableRes;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.a2048.databinding.FragmentScoreJoueurBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +20,14 @@ public class ScoreJoueur extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private FragmentScoreJoueurBinding binding;
+    private String classement;
+    private String name;
+    private int score;
+    private int bestCase;
 
     public ScoreJoueur() {
         // Required empty public constructor
@@ -32,33 +37,43 @@ public class ScoreJoueur extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param classement
+     * @param name
+     * @param score
+     * @param bestCase
      * @return A new instance of fragment score_joueur.
      */
     // TODO: Rename and change types and number of parameters
-    public static ScoreJoueur newInstance(String param1, String param2) {
+    public static ScoreJoueur newInstance(String classement, String name, int score, int bestCase) {
         ScoreJoueur fragment = new ScoreJoueur();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("CLASSEMENT", classement);
+        args.putString("NAME", name);
+        args.putInt("SCORE", score);
+        args.putInt("BESTCASE",bestCase);
         fragment.setArguments(args);
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        classement = getArguments().getString("CLASSEMENT", "");
+        name = getArguments().getString("NAME", "");
+        score = getArguments().getInt("SCORE", 0);
+        bestCase = getArguments().getInt("BESTCASE", 0);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score_joueur, container, false);
+        binding = FragmentScoreJoueurBinding.inflate(inflater, container, false);
+        binding.textClassement.setText(classement);
+        binding.textName.setText(name);
+        binding.textScore.setText(score);
+        //binding.imageBestCase.setImageResource();
+        return binding.getRoot();
     }
 }
