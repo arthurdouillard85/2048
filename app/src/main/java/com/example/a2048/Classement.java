949cd838b46644707c405b2c1d62c2109a12e9e9
Arthur <arthur.douillard85@gmail.com>
2024-04-16 11:43:03 +0200

    correction Merge de save_score

--- app/src/main/java/com/example/a2048/Classement.java
+++ app/src/main/java/com/example/a2048/Classement.java
@@ -1,22 +1,22 @@
 package com.example.a2048;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.fragment.app.Fragment;
-        import androidx.fragment.app.FragmentManager;
-        import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 
-        import com.example.a2048.databinding.ActivityClassementBinding;
-        import com.google.gson.Gson;
-        import com.google.gson.reflect.TypeToken;
+import com.example.a2048.databinding.ActivityClassementBinding;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-        import java.lang.reflect.Type;
-        import java.util.ArrayList;
-        import java.util.Collections;
-        import java.util.Comparator;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Classement extends AppCompatActivity {
 
